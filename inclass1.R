65d9ba018f058dadd9d31b3ce85916895d8e0c57
justJuni <junkim@Juns-MacBook-Pro-2.local>
2014-09-03 14:10:43 -0400

    Class work 1

--- inclass1.R
+++ inclass1.R
@@ -1,12 +1,19 @@
 # sets working directory to my local
 setwd("~/Desktop/CS3654/CMDA")
+# Read cars1.cs
 csvdata<- read.table('cars1.csv', sep=",", header=T)
+# 3. dimensions
 dim(csvdata)
+# 4.value of 2,2
 var1<- csvdata[2,2]
 var1
+# 5.variable names
 names(csvdata)
+# 6.first and second columns
 csvdata[1]
 csvdata[2]
+# 7.speed variable
 SPEED<- csvdata$speed
+# 8.out row 15
 print(SPEED)
 csvdata[15,]
